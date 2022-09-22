05c1f89882d173c179c9ec93e9b722f17db2fca7
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2022-09-22 08:40:11 +0200

    Review on dialog boxes and few new things abt vars

---
+++ dialog_boxes/quick_review/script.js
@@ -0,0 +1,7 @@
+// alert('Hello, world!');
+
+// let x = parseFloat(prompt('Podaj liczbę'));
+
+let x = confirm("To jest okienko confirm")
+
+document.write(`${x} jest typu ${typeof x}`);
---
+++ variables/script.js
@@ -0,0 +1,25 @@
+const stala = 8
+var zmienna1 = 6
+let zmienna2 = 9
+
+// ####################
+// # TAK SIĘ NIE ROBI #
+// ####################
+// zmienna1 = 6
+// zmienna2 = 9
+
+console.log(`
+    Stała wynosi ${stala}
+    Zmienna globalna wynosi ${zmienna1}
+    Zmienna lokalna wynosi ${zmienna2}
+`)
+
+// stala = 16;
+zmienna1 = 12
+zmienna2 = 18
+
+console.log(`
+    Stała wynosi ${stala}
+    Zmienna globalna wynosi ${zmienna1}
+    Zmienna lokalna wynosi ${zmienna2}
+`)
