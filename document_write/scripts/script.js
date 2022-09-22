69d9a96fa4352ca8b44cb0b4c5f69f42500a3040
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2022-09-22 08:26:28 +0200

    document.write

---
+++ document_write/scripts/script.js
@@ -0,0 +1,28 @@
+document.write(`
+    <p>Lista nieuporządkowana</p>
+    <ul>
+        <li>Element 1</li>
+        <li>Element 2</li>
+        <li>Element 3</li>
+        <li>Element 4</li>
+        <li>Element 5</li>
+    </ul>
+`)
+
+document.write(`
+    <p>Lista uporządkowana</p>
+    <ol>
+        <li>Element 1</li>
+        <li>Element 2</li>
+        <li>Element 3</li>
+        <li>Element 4</li>
+        <li>Element 5</li>
+    </ol>
+`)
+
+document.write(`
+    <p>Obrazek</p>
+    <img src="https://c.tenor.com/pMhSj9NfCXsAAAAd/saul-goodman-better-call-saul.gif">
+`)
+
+console.log(document);
